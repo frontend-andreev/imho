eefe212e39b32124a7d6796e4b641eb3a623f0a2
Daniil <62702483+andreev781@users.noreply.github.com>
2024-05-24 15:05:57 +0300

    Update state.js

--- src/store/modules/users/state.js
+++ src/store/modules/users/state.js
@@ -1,5 +1,22 @@
 const state = {
-    listOfUsers: [],
+        listOfUsers: [{
+        id: 1,
+        login: '123',
+        password: '123',
+        name: 'Даниил',
+        surname: 'Андреев',
+        roles: ['администратор'],
+        availableCountries: ['RUS']
+
+    }, {
+        id: 2,
+        login: 'test',
+        password: 'test',
+        name: 'Кто-то',
+        surname: 'Кто-то',
+        roles: ['администратор'],
+        availableCountries: ['RUS']
+    }],
     currentUser: null
 }
 
